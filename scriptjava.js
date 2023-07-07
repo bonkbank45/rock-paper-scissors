0be127a451a967f9dbca1bcd97282871acf935ae
bonkbank45 <bankforever22@gmail.com>
2023-07-07 20:43:29 +0700

    Add function game()

--- scriptjava.js
+++ scriptjava.js
@@ -14,24 +14,54 @@ function getComputerChoice() {
 }
 
 function playRound(playerSelection, computerSelection) {
-    const playerSelection = playerSelection.replace(playerSelection.charAt(0), playerSelection.charAt(0).toUpperCase());
+    const playerSelectionChoice = playerSelection.replace(playerSelection.charAt(0), playerSelection.charAt(0).toUpperCase());
     let result;
-    if (playerSelection === "Rock" && computerSelection === "Scissor") {
-        result = `You Win! ${playerSelection} beats ${computerSelection}`;
+    if (playerSelectionChoice === "Rock" && computerSelection === "Scissor") {
+        result = `You Win! ${playerSelectionChoice} beats ${computerSelection}`;
     }
-    else if (playerSelection === "Scissor" && computerSelection === "Paper") {
-        result = `You Win! ${playerSelection} beats ${computerSelection}`;
+    else if (playerSelectionChoice === "Scissor" && computerSelection === "Paper") {
+        result = `You Win! ${playerSelectionChoice} beats ${computerSelection}`;
     }
-    else if (playerSelection === "Paper" && computerSelection === "Rock") {
-        result = `You Win! ${playerSelection} beats ${computerSelection}`;
+    else if (playerSelectionChoice === "Paper" && computerSelection === "Rock") {
+        result = `You Win! ${playerSelectionChoice} beats ${computerSelection}`;
     }
-    else if (playerSelection === computerSelection) {
-        result = `Withdrawn ${playerSelection} equals ${computerSelection} !`;
+    else if (playerSelectionChoice === computerSelection) {
+        result = `Withdrawn ${playerSelectionChoice} equals ${computerSelection} !`;
     }
     else {
-        result = `You Lose ${computerSelection} beats ${playerSelection}`;
+        result = `You Lose ${computerSelection} beats ${playerSelectionChoice}`;
     }
     return result;
+}
+
+function game() {
+    let playerScore = 0;
+    let computerScore = 0;
+
+    for (let i = 0; i < 5; i++ ) {
+        let playerChoice = prompt("Rock Paper Scissor!")
+        let computerSelection = getComputerChoice();
+        let result = playRound(playerChoice, computerSelection);
+        if (result.match(/You Win!.+/g)) {
+            console.log(result);
+            playerScore++
+        } else if (result.match(/You Lose.+/g)) {
+            console.log(result);
+            computerScore++;
+        } else {
+            console.log(result);
+        }
+    }
+
+    if (playerScore > computerScore) {
+        return `You Win! with score ${playerScore}-${computerScore}`;
+    }
+    else if (computerScore > playerScore) {
+        return `You Lose with score ${playerScore}-${computerScore}`;
+    }
+    else {
+        return `Withdrawn with score ${playerScore}-${computerScore}`
+    }
 }
 
 // rock beats scissor
