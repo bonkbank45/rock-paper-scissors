b30f42ad61578b64f1bb6ce9eb4c7ec57684c76d
bonkbank45 <bankforever22@gmail.com>
2023-07-09 16:21:45 +0700

    Add log box

---
+++ main.js
@@ -0,0 +1,71 @@
+function getComputerChoice() {
+    const choiceComputer = Math.floor(Math.random() * 3) + 1;
+    let result;
+    if (choiceComputer === 1) {
+        result = "Rock";
+    }
+    else if (choiceComputer === 2) {
+        result = "Paper";
+    }
+    else {
+        result = "Scissor";
+    }
+    return result;
+}
+
+function playRound(event) {
+
+    const playerSelection = event.target.innerText;
+    const computerSelection = getComputerChoice();
+    let result = "";
+
+    if (playerSelection === "Rock" && computerSelection === "Scissor") {
+        result = `You Win! ${playerSelection} beats ${computerSelection}`;
+    }
+    else if (playerSelection === "Scissor" && computerSelection === "Paper") {
+        result = `You Win! ${playerSelection} beats ${computerSelection}`;
+    }
+    else if (playerSelection === "Paper" && computerSelection === "Rock") {
+        result = `You Win! ${playerSelection} beats ${computerSelection}`;
+    }
+    else if (playerSelection === computerSelection) {
+        result = `Draw, ${playerSelection} equals ${computerSelection} !`;
+    }
+    else {
+        result = `You Lose ${computerSelection} beats ${playerSelection}`;
+    }
+
+    game(result);
+}
+
+function game(playRound) {
+    let playerScore = 0;
+    let computerScore = 0;
+    const log = document.querySelector(".log")
+    const para = document.createElement("p");
+
+    if (playRound.match(/You Win!/)) {
+        console.log("Win");
+    } else {
+        console.log("Loss")
+    }
+    /*
+    if (playerScore > computerScore) {
+        return `You Win! with score ${playerScore}-${computerScore}`;
+    }
+    else if (computerScore > playerScore) {
+        return `You Lose with score ${playerScore}-${computerScore}`;
+    }
+    else {
+        return `Withdrawn with score ${playerScore}-${computerScore}`
+    }
+    */
+    para.innerHTML = playRound;
+    log.appendChild(para);
+}
+
+const buttons = document.querySelectorAll("button");
+console.log(buttons);
+buttons.forEach( (button) => {
+    button.addEventListener("click", playRound)
+})
